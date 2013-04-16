edb27e1c627a34cbcd5296e8bedcf64109cdbba9
nn959703 <nn959703@gmail.com>
2013-04-16 18:58:53 +0800

    use groupby based on SO answer: http://stackoverflow.com/questions/15997379/pandas-pivot-table-subtotals-per-each-cols

--- report.py
+++ report.py
@@ -42,8 +42,7 @@ def parse_report(tryp_file):
     
 
 
-if True:
-#if __name__ == '__main__':
+if __name__ == '__main__':
     reports = parse_report("daily_sales.tryp")
     for report in reports:
         conn = data_connection(report["conn_str"])
@@ -53,7 +52,6 @@ if True:
             return sum([5])
 
         rows = ['region', 'area', 'distributor', 'salesrep_name']
-        rows = ['region']
         aggs = ['net_sales', 'sales_order_id_count']
         agg = {'net_sales':np.sum,'sales_order_id_count': fuck}
 
@@ -83,3 +81,92 @@ if True:
             for i, col in enumerate(row):
                 ws.write(row_index, i, col)
         wb.save('SHIT.xls')
+
+import numpy as np
+import pandas as pd
+
+
+groups = ['region', 'area', 'distributor', 'salesrep_name']
+agg = ['net_sales', 'sales_order_id_count']
+
+total = pd.DataFrame({'region': ['Grand Total'],
+                      'sales_order_id_count': df['sales_order_id_count'].sum(),
+                      'net_sales': df['net_sales'].sum()})
+total['total_rank'] = 1
+def label(ser):
+    return '{s} Total'.format(s=ser)
+
+dfs = [total]
+for i in range(1,len(groups)):
+    parent_group = groups[:i]
+    child_group = groups[i]
+    group_total = df.groupby(parent_group, as_index=False).sum()
+    group_total[child_group] = group_total[parent_group[-1]].apply(label)
+    group_total[parent_group[-1] + '_rank'] = 1
+    dfs.append(group_total)
+
+dfs.append(df.groupby(groups, as_index=False).sum())
+
+# UNION the DataFrames into one DataFrame
+result = pd.concat(dfs)
+# Replace NaNs with empty strings
+result.fillna(dict([(x,'') for x in groups]), inplace=True)
+
+groups_sort = []
+for x in reversed(groups[:-1]):
+    groups_sort.append(result[x + '_rank'].rank())
+    groups_sort.append(result[x].rank())
+groups_sort.append(result['total_rank'].rank())
+
+# Reorder the rows
+sorter = np.lexsort((groups_sort))
+result = result.take(sorter)
+result = result.reindex(columns=groups + aggs)
+print result['net_sales']
+result1 = result.to_dict()
+result.to_csv('/tmp/test1.csv')
+result.to_excel('SHIT1.xls')
+#print(result.to_string(index=False))
+
+
+
+    
+    
+#total = pd.DataFrame({'region': ['Grand Total'],
+#                      'sales_order_id_count': df['sales_order_id_count'].sum(),
+#                      'net_sales': df['net_sales'].sum()})
+#
+#region_total = df.groupby(['region'], as_index=False).sum()
+#region_total['area'] = region_total['region'].apply(label)
+#
+#area_total = df.groupby(['region', 'area'], as_index=False).sum()
+#area_total['distributor'] = area_total['area'].apply(label)
+#
+#dist_total = df.groupby( ['region', 'area', 'distributor'], as_index=False).sum()
+#dist_total['salesrep_name'] = dist_total['distributor'].apply(label)
+#
+#rep_total = df.groupby(['region', 'area', 'distributor', 'salesrep_name'], as_index=False).sum()
+#
+#total['total_rank'] = 1
+#region_total['region_rank'] = 1
+#area_total['area_rank'] = 1
+#
+## UNION the DataFrames into one DataFrame
+#result = pd.concat([total, region_total, area_total, dist_total, rep_total])
+#
+## Replace NaNs with empty strings
+#result.fillna({'region': '', 'area': '', 'distributor': '', 'salesrep_name': ''}, inplace=True)
+## Reorder the rows
+#sorter = np.lexsort((
+#        result['distributor'].rank(),
+#        result['area_rank'].rank(),
+#        result['area'].rank(),
+#        result['region_rank'].rank(),
+#        result['region'].rank(),
+#        result['total_rank'].rank()))
+#result = result.take(sorter)
+#result = result.reindex(columns=['region', 'area', 'distributor', 'salesrep_name', 'net_sales', 'sales_order_id_count'])
+##print(result.to_string(index=False))
+#result2 = result.to_dict()
+#result.to_csv('/tmp/test2.csv')
+#print result1==result2
