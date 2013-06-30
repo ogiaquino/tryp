a85756da17d8cb24a5656aa804a3ba53c6bed0d6
ogiaquino <ogiaquino@gmail.com>
2013-06-30 17:17:28 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+requires = ['pandas', 'xlwt', 'psycopg2']
+
+setup(name='tryp',
+      version='0.0',
+      description='tryp',
+      install_requires=requires,
+     )
