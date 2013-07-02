32e551bd0c11986df2cc8d49659f1cf90d46c14d
ogiaquino <ogiaquino@gmail.com>
2013-07-02 19:37:43 +0800

    update refactor

--- inventory_by_sku/refactor.sql
+++ inventory_by_sku/refactor.sql
@@ -1,8 +1,9 @@
-with stock_on_hand AS (
+with 
+stock_on_hand AS (
     SELECT
+        dim_products.id AS product_id,
         dim_calendar.date,
         dim_sites.combo,
-        dim_products.id AS product_id,
         dim_products.status,
         dim_products.type,
         (coalesce(stocks.quantity_on_hand, 0)::float / dim_products.conversion_factor)::numeric AS soh_converted_quantity,
@@ -17,31 +18,55 @@ with stock_on_hand AS (
     JOIN 
         dim_calendar ON calendar_id = dim_calendar.id
     WHERE                                                                      
-        dim_products.type != 'Point of sales'
-        AND
         dim_calendar.date = now()::date
+        AND dim_products.status IN ('Active', 'Discontinued', 'To be discontinued')
+        AND dim_products.type IN ('New', 'Promotion', 'Standard')
+),
+
+sales AS (
+    SELECT
+        dim_products.id AS product_id,
+        dim_calendar.date,
+        dim_sites.combo,
+        dim_products.status,
+        dim_products.type,
+        (coalesce(invoices.delivered_quantity, 0)::float / dim_products.conversion_factor)::numeric AS sales_converted_quantity,
+        coalesce(invoices.standard_value, 0) AS sales_standard_value,
+        (coalesce(invoices.delivered_quantity, 0)::float / dim_products.conversion_factor)::numeric * dim_products.weight AS sales_volume
+    FROM
+        mv_fact_invoices as invoices
+    JOIN 
+        dim_sites ON site_id = dim_sites.id 
+    JOIN 
+        dim_products ON product_id = dim_products.id                          
+    JOIN 
+        dim_calendar ON calendar_id = dim_calendar.id
+    WHERE                                                                      
+        dim_calendar.date BETWEEN (now()::date - '1 day'::interval - '74 day'::interval) AND (now()::date - '1 day'::interval)
+        AND dim_products.status IN ('Active', 'Discontinued', 'To be discontinued')
+        AND dim_products.type IN ('New', 'Promotion', 'Standard')
 )
 
-SELECT
-    soh.combo,
-    soh.product_id,
-	sum(soh.soh_standard_value ) AS soh_standard_value,
-    sum(soh.soh_converted_quantity) AS soh_converted_quantity,
-    sum((soh.soh_converted_quantity * product.weight) / 1000) AS soh_volume
-FROM
-    stock_on_hand AS soh
-JOIN 
-    dim_products as product 
-ON 
-    product_id = product.id                    
-    ,
-    (
-        SELECT
-             now()::date AS date
-    ) AS t
-WHERE
-    1 = 1
-    AND soh.status IN ('Active', 'Discontinued', 'To be discontinued')
-    AND soh.type IN ('New', 'Promotion', 'Standard')
-    AND soh.date = t.date
-GROUP BY 1, 2
+SELECT * FROM sales FULL JOIN stock_on_hand using(product_id,date,combo,status,type) LIMIT 5;
+
+    SELECT
+        dim_products.id AS product_id,
+        dim_calendar.date,
+        dim_sites.combo,
+        dim_products.status,
+        dim_products.type,
+        (coalesce(invoices.delivered_quantity, 0)::float / dim_products.conversion_factor)::numeric AS sales_converted_quantity,
+        coalesce(invoices.standard_value, 0) AS sales_standard_value,
+        (coalesce(invoices.delivered_quantity, 0)::float / dim_products.conversion_factor)::numeric * dim_products.weight AS sales_volume
+    FROM
+        mv_fact_invoices as invoices
+    JOIN 
+        dim_sites ON site_id = dim_sites.id 
+    JOIN 
+        dim_products ON product_id = dim_products.id                          
+    JOIN 
+        dim_calendar ON calendar_id = dim_calendar.id
+    WHERE                                                                      
+        dim_calendar.date BETWEEN (now()::date - '1 day'::interval - '74 day'::interval) AND (now()::date - '1 day'::interval)
+        AND dim_products.status IN ('Active', 'Discontinued', 'To be discontinued')
+        AND dim_products.type IN ('New', 'Promotion', 'Standard')
