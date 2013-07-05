066f2b7f23061d0523060cec3fbce341576722be
ogiaquino <ogiaquino@gmail.com>
2013-07-05 23:14:48 +0800

    ENH: Add tryp file schema to be used for validating.
    Updates to #12

--- jsonparser.py
+++ jsonparser.py
@@ -1,6 +1,19 @@
 import json
 
 
+schema = {
+    "crosstab": {
+        "columns": [],
+        "rows": [],
+        "values": [],
+        "rows_totals": [],
+        "labels": {
+            "values": {}
+        }
+    }
+}
+
+
 def parse_tryp(tryp_file):
     with open(tryp_file) as data_file:
         data = json.load(data_file)
