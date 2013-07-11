92caeb3b36092149da345a25f9a1baf0d47e8688
ogiaquino <ogiaquino@gmail.com>
2013-07-11 21:07:50 +0800

    TST: Add more test cases in test_excel.py

--- tryp/tests/test_excel.py
+++ tryp/tests/test_excel.py
@@ -17,7 +17,36 @@ class TestExcel(unittest.TestCase):
 
         ct = Dataset(df, rows, columns, values, rows_total).crosstab
 
+        merge_labels_expected_result = {0: [(0, 0, u''),
+                                            (1, 7, u'Central'),
+                                            (8, 14, u'East')],
+
+                                        1: [(0, 0, u''),
+                                            (1, 1, u'Central'),
+                                            (2, 4, u'Butterworth'),
+                                            (5, 7, u'Ipoh'),
+                                            (8, 8, u'East'),
+                                            (9, 11, u'JB'),
+                                            (12, 14, u'PJ')],
+
+                                        2: [(0, 0, u''),
+                                            (1, 1, u'Central'),
+                                            (2, 2, u'Butterworth'),
+                                            (3, 3, u'HINMARKETING'),
+                                            (4, 4, u'KWANGHENG'),
+                                            (5, 5, u'Ipoh'),
+                                            (6, 6, u'CORESYN'),
+                                            (7, 7, u'SGHEDERAN'),
+                                            (8, 8, u'East'),
+                                            (9, 9, u'JB'),
+                                            (10, 10, u'LEIWAH'),
+                                            (11, 11, u'WONDERF&B'),
+                                            (12, 12, u'PJ'),
+                                            (13, 13, u'HEBAT'),
+                                            (14, 14, u'PENGEDAR')]}
+
         labels = merge_labels(ct.index, 3, 2)
+        assert labels == merge_labels_expected_result
         assert len(labels) == len(rows)
         for i in labels:
             assert len(ct.index) - 1 == labels[i][-1][1]
