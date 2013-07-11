e4455794d3efa9fe8155e857d28c1d0931c343f6
Low Kian Seong <kianseong@gmail.com>
2013-07-11 14:58:01 +0800

    removing pep8 violations

--- styles.py
+++ styles.py
@@ -5,9 +5,10 @@ def borders_style(borders):
     brd = Borders()
     brd.bottom = borders["bottom"]
     brd.left = borders["left"]
-    brd.right= borders["right"]
+    brd.right = borders["right"]
     brd.top = borders["top"]
     return brd
+
 
 def get_styles(reportname, axis):
     module = __import__('%s.styles' % reportname, fromlist=[axis])
@@ -16,13 +17,15 @@ def get_styles(reportname, axis):
     for name, style in styles.iteritems():
         xf_str = 'font: name %(name)s, color %(color)s,' \
                  'bold %(bold)s, height %(height)s;' \
-                 'pattern: pattern solid, fore-colour %(background)s;' % styles[name]['font']
+                 'pattern: pattern solid, fore-colour %(background)s;' \
+                 % styles[name]['font']
         xf_str = xf_str + styles[name]['alignment']
         exf = easyxf(xf_str)
         exf.borders = borders_style(styles[name]['border'])
         exf.num_format_str = styles[name]['number_format_str']
         crosstab_styles[name] = exf
     return crosstab_styles
+
 
 def get_labels(reportname, axis):
     try:
