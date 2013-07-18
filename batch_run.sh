313e71561c4d0e1d082161ff3e49aaa5fbf4440b
ogiaquino <ogiaquino@gmail.com>
2013-07-19 01:22:35 +0800

    ENH: fixed wrong directory

--- batch_run.sh
+++ batch_run.sh
@@ -1,6 +1,6 @@
 pyclean .
 rm *.xls
-trypgen -f inventory_by_sku.tryp -d inventory_by_sku.csv -o inventory_by_sku.xls
-trypgen -f dsr_excel.tryp -d dsr_excel.csv -o dsr_excel.xls 
-trypgen -f bsr_excel.tryp -d bsr_excel.csv -o bsr_excel.xls 
-trypgen -f inventory_by_dist.tryp -d inventory_by_dist.csv -o inventory_by_dist.xls
+trypgen -f reports/inventory_by_sku.tryp -d reports/inventory_by_sku.csv -o inventory_by_sku.xls
+trypgen -f reports/dsr_excel.tryp -d reports/dsr_excel.csv -o dsr_excel.xls 
+trypgen -f reports/bsr_excel.tryp -d reports/bsr_excel.csv -o bsr_excel.xls 
+trypgen -f reports/inventory_by_dist.tryp -d reports/inventory_by_dist.csv -o inventory_by_dist.xls
