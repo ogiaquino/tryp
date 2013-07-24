f11835e6612b00b6dd6cad77156ff9203ce4360e
Low Kian Seong <kianseong@gmail.com>
2013-07-24 10:07:41 +0800

    Update batch_run.sh
    Remove hard code from run_batch.sh

--- batch_run.sh
+++ batch_run.sh
@@ -1,6 +1,8 @@
 pyclean .
 rm -f *.xls
-trypgen -f reports/inventory_by_sku.tryp -d reports/inventory_by_sku.csv -o inventory_by_sku.xls
-trypgen -f reports/dsr_excel.tryp -d reports/dsr_excel.csv -o dsr_excel.xls 
-trypgen -f reports/bsr_excel.tryp -d reports/bsr_excel.csv -o bsr_excel.xls 
-trypgen -f reports/inventory_by_dist.tryp -d reports/inventory_by_dist.csv -o inventory_by_dist.xls
+
+for report in `ls report/*.tryp`
+do
+  base_file=`basename $report`
+  trygen -f $report -d ${report%.*}.csv -o ${base_file%.*}.xls 
+done
