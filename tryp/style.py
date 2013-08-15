3d758405aea35b6b36823c2fba68b1fe11aafc5b
ogiaquino <ogiaquino@gmail.com>
2013-08-15 18:17:25 +0800

    ENH: get xyz in get_values_styles

--- tryp/style.py
+++ tryp/style.py
@@ -2,9 +2,17 @@ import xlrd
 
 template = "report/inventory_by_sku_templates.xls"
 
+
 def get_values_styles(ct):
     wb = xlrd.open_workbook(template, formatting_info=True)
     ws = wb.sheet_by_index(0)
-    row = 4
-    col = 4
-    print ws.cell(4,4).value
+
+    yaxis = [''] + ct.visible_yaxis_summary + [ct.yaxis[-1]]
+    xaxis = [''] + ct.xaxis
+    for i, y in enumerate(yaxis):
+        col = -1
+        for x in xaxis:
+            for z in ct.zaxis:
+                col = col + 1
+                print y, x, z, ws.cell(i + len(ct.xaxis) + 1,
+                                       col + len(ct.yaxis)).value
