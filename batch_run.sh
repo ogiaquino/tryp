fc4932f5b25e706cee2cf54f881635853385752c
ogiaquino <ogiaquino@gmail.com>
2013-08-19 23:41:41 +0800

    ENH: Add template arg

--- batch_run.sh
+++ batch_run.sh
@@ -4,5 +4,5 @@ rm -f *.xls
 for report in `ls report/*.tryp`
 do
   base_file=`basename $report`
-  trypgen -f $report -d ${report%.*}.csv -o ${base_file%.*}.xls 
+  trypgen -f $report -d ${report%.*}.csv -o ${base_file%.*}.xls -t ${report%.*}_template.xls
 done
