ca73a996ddb5a19c2f06e08f23dda4420ee7f73f
ogiaquino <ogiaquino@gmail.com>
2013-08-22 21:44:57 +0800

    ENH: support overwriting of style

--- tryp/template.py
+++ tryp/template.py
@@ -2,14 +2,15 @@ from xlrd import open_workbook
 from xlwt import easyxf, Borders, Pattern, Style
 
 colour = {
-    46: "lavender",
-    43: "light-yellow",
-    51: "gold",
-    64: "white",
-    50: "lime",
+    8: "black",
+    10: "red",
     13: "yellow",
     40: "sky-blue",
-    10: "red"
+    43: "light-yellow",
+    46: "lavender",
+    50: "lime",
+    51: "gold",
+    64: "white",
 }
 
 
@@ -37,13 +38,13 @@ class Template(object):
         for row in range(0, crosstab_row):
             for col in range(len(self.ws.row(row))):
                 h = (row, col, self.ws.row(row)[col].value,
-                     self.__get_styles(row - crosstab_row, col))
+                     self.get_styles(row - crosstab_row, col))
                 headers.append(h)
         return headers
 
     def __get_ct_corner_styles(self):
         styles = {}
-        return self.__get_styles(0, 0)
+        return self.get_styles(0, 0)
 
     def __get_values_styles(self):
         yaxis = [''] + self.ct.visible_yaxis_summary + [self.ct.yaxis[-1]]
@@ -55,8 +56,9 @@ class Template(object):
             for x in xaxis:
                 for z in self.ct.zaxis:
                     col = col + 1
-                    sty = self.__get_styles(i + len(self.ct.xaxis) + 1,
-                                            col + len(self.ct.yaxis))
+                    sty = self.get_styles(i + len(self.ct.xaxis) + 1,
+                                          col + len(self.ct.yaxis))
+                    sty.z = z
                     styles[(y, x, z)] = sty
         return styles
 
@@ -69,14 +71,14 @@ class Template(object):
             for j in range(len(yaxis)):
                 col = j
                 row = i + len(self.ct.xaxis) + 1
-                sty = self.__get_styles(row, col)
+                sty = self.get_styles(row, col)
                 styles[(y, j)] = sty
         return styles
 
     def __get_values_labels_styles(self):
         styles = {}
         for i in range(len(self.ct.zaxis)):
-            sty = self.__get_styles(len(self.ct.xaxis), i + len(self.ct.yaxis))
+            sty = self.get_styles(len(self.ct.xaxis), i + len(self.ct.yaxis))
             styles[self.ct.zaxis[i]] = sty
         return styles
 
@@ -90,44 +92,50 @@ class Template(object):
             for i, x in enumerate(xaxis):
                 for j, z in enumerate(self.ct.zaxis):
                     col = col + 1
-                    styles[(h, x, z)] = self.__get_styles(h, col)
+                    styles[(h, x, z)] = self.get_styles(h, col)
 
         return styles
 
-    def __get_styles(self, row, col):
+    def get_styles(self, row, col, overwrite={}):
         crosstab_row, crosstab_col = self.crosstab_loc
-        row = row + crosstab_row
-        col = col + crosstab_col
-        xf = self.wb.xf_list[self.ws.cell_xf_index(row, col)]
+        xf = self.wb.xf_list[self.ws.cell_xf_index(row + crosstab_row,
+                                                   col + crosstab_col)]
         xfval = dict(self.__font(xf) +
                      self.__pattern(xf) +
                      self.__alignment(xf) +
                      self.__borders(xf))
-        xfstr = 'font: name %(name)s, height %(height)s, bold %(bold)s;'\
+        for k in overwrite:
+            xfval[k] = overwrite[k]
+
+        xfstr = 'font: name %(name)s, colour %(colour_index)s,' \
+                'height %(height)s, bold %(bold)s;'\
                 'pattern: pattern solid, fore-colour %(forecolour)s;'\
                 'alignment: vertical %(vertical)s, horizontal %(horizontal)s;'\
                 'borders : bottom %(bottom)s, left %(left)s,'\
                 'right %(right)s, top %(top)s' % xfval
         style = easyxf(xfstr)
-        style.num_format_str = self.__number_format(xf)
 
         style.label = ''
-        value = self.ws.cell(row, col).value
+        value = self.ws.cell(row + crosstab_row, col + crosstab_col).value
         if isinstance(value, basestring):
             if '[' in value and ']' in value:
                 value = value.split('[')[1]
                 value = value.replace(']', '')
                 style.label = value
-        style.row_height = self.ws.rowinfo_map[row].height
-        style.column_width = self.ws.computed_column_width(col)
+
+        style.row = row
+        style.col = col
+        style.row_height = self.ws.rowinfo_map[row + crosstab_row].height
+        style.column_width = self.ws.computed_column_width(col + crosstab_col)
+        style.num_format_str = self.__number_format(xf)
         return style
 
     def __font(self, xf):
-        name = self.wb.font_list[xf.font_index].name
-        height = self.wb.font_list[xf.font_index].height
-        bold = self.wb.font_list[xf.font_index].weight
-        bold = 'on' if bold == 700 else 'off'
-        return (('name', name), ('height', height), ('bold', bold))
+        font = self.wb.font_list[xf.font_index]
+        bold = 'on' if font.weight == 700 else 'off'
+        colour_index = colour.get(font.colour_index, 8)
+        return (('name', font.name), ('height', font.height), ('bold', bold),
+                ('colour_index', colour_index))
 
     def __pattern(self, xf):
         forecolour = colour[xf.background.pattern_colour_index]
