df93ec9343e599759462245b3555be86bc3f3c48
ogiaquino <ogiaquino@gmail.com>
2013-08-23 17:49:29 +0800

    ENH: Initial commit for adding postgres datasource

--- tryp/tryp.py
+++ tryp/tryp.py
@@ -1,13 +1,31 @@
+"""Usage:
+    tryp.py -f tryp_file -d csv_file -o output_file  -t template_file
+    tryp.py -f tryp_file -o output_file -t template_file\
+ --connstring=<arg> --sqlfile=<arg> [--sqlparams=<arg>]...
+
+   Options:
+          -f tryp_file.
+          -d csv_file.
+          -o out_putfile
+          -t template_file
+          --connstring connstring
+          --sqlfile sql_file
+          --sqlparams sqlparams
+"""
 import os
+import psycopg2
 from pandas.io.parsers import read_csv
+from pandas.io import sql as psql
 from parser import parse
 from crosstab import Crosstab
 
 
 class CrosstabMetaData(object):
-    def __init__(self, tryp_file, template_file, csv_file, output_file):
+    def __init__(self, tryp_file, template_file, output_file, csv_file,
+                 connstring, sql_file, sqlparams):
         self.report = parse(tryp_file, 'crosstab')
-        self.source_dataframe = self.data_frame(csv_file)
+        self.source_dataframe = self.data_frame(csv_file, connstring, sql_file,
+                                                sqlparams)
         self.xaxis = self.report['xaxis']
         self.yaxis = self.report['yaxis']
         self.zaxis = self.report['zaxis']
@@ -27,32 +45,37 @@ class CrosstabMetaData(object):
         if os.path.exists(extmodule):
             return (tryp_filename, extmodule)
 
-    def data_frame(self, csv_file):
-        df = read_csv(csv_file)
+    def data_frame(self, csv_file, connstring, sql_file, sqlparams):
+        if csv_file == 'csv':
+            df = read_csv(csv_file)
+        else:
+            query = open(sql_file).read()
+            #query = query % sqlparams
+            conn = psycopg2.connect(self.report['connstring'])
+            df = psql.frame_query(query, con=conn)
         return df
 
 
 class Tryp(object):
-    def __init__(self, tryp_file, template_file, csv_file, output_file):
-        ctmeta = CrosstabMetaData(tryp_file, template_file, csv_file,
-                                  output_file)
+    def __init__(self, tryp_file, template_file, output_file, csv_file,
+                 connstring, sql_file, sqlparams):
+        ctmeta = CrosstabMetaData(tryp_file, template_file, output_file,
+                                  csv_file, connstring, sql_file, sqlparams)
         self.crosstab = Crosstab(ctmeta)
 
 
 def main():
-        import argparse
-        parser = argparse.ArgumentParser(description='Generate Excel File.')
-        parser.add_argument('-f')
-        parser.add_argument('-t')
-        parser.add_argument('-d')
-        parser.add_argument('-o')
-        args = parser.parse_args()
-        tryp_file = args.f
-        template_file = args.t
-        output_file = args.o
-        csv_file = args.d
-        Tryp(tryp_file, template_file, csv_file, output_file). \
-                crosstab.to_excel()
+    from docopt import docopt
+    args = docopt(__doc__, version='Tryp 0.0')
+    tryp_file = args['-f']
+    template_file = args['-t']
+    output_file = args['-o']
+    csv_file = args['-d']
+    connstring = args['--connstring']
+    sql_file = args['--sqlfile']
+    sqlparams = args['--sqlparams']
+    Tryp(tryp_file, template_file, output_file, csv_file, connstring,
+         sql_file, sqlparams).crosstab.to_excel()
 
 if __name__ == '__main__':
     main()
