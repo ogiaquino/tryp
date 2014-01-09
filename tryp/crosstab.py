aeef660b7f5863b83d8eb098c4749adfaf069851
ogiaquino <ogiaquino@gmail.com>
2014-01-09 17:59:56 +0800

    ENH: refactor crosstab.py

--- tryp/crosstab.py
+++ tryp/crosstab.py
@@ -1,57 +1,51 @@
 import imp
 import pandas as pd
 import numpy as np
+from itertools import cycle, islice
 
-from common import roundrobin
-from excel import to_excel as to_excel
-
+def roundrobin(*iterables):
+    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+    # Recipe credited to George Sakkis
+    pending = len(iterables)
+    nexts = cycle(iter(it).next for it in iterables)
+    while pending:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            pending -= 1
+            nexts = cycle(islice(nexts, pending))
 
 class Crosstab(object):
-    def __init__(self, metadata):
+    def __init__(self, xaxis, yaxis, zaxis, xaxis_total, yaxis_total,
+                 source_dataframe, extmodule=None):
         self.coordinates = {}
-        self.metadata = metadata
-        self.xaxis = metadata.xaxis
-        self.yaxis = metadata.yaxis
-        self.zaxis = metadata.zaxis
-        self.visible_xaxis_summary = metadata.visible_xaxis_summary
-        self.visible_yaxis_summary = metadata.visible_yaxis_summary
-        if not metadata.source_dataframe.empty:
-            self.dataframe = self.__crosstab(metadata.source_dataframe,
-                                             self.xaxis,
-                                             self.yaxis,
-                                             self.zaxis)
-            self.__extend(metadata.extmodule)
-        else:
-            self.dataframe = metadata.source_dataframe
+        self.xaxis = xaxis
+        self.yaxis = yaxis
+        self.zaxis = zaxis
+        self.xaxis_total = xaxis_total
+        self.yaxis_total = yaxis_total
 
-    def to_excel(self):
-        to_excel(self)
-
-    def __crosstab(self, source_dataframe, xaxis, yaxis, zaxis):
         df = source_dataframe.groupby(xaxis + yaxis).sum()
         df = df[zaxis].unstack(xaxis)
         if xaxis:
-            df = self.__xaxis_summary(source_dataframe,
-                                      xaxis,
-                                      yaxis,
-                                      zaxis,
-                                      df)
-        return self.__yaxis_summary(yaxis, df)
+            df = self._xaxis_total(source_dataframe, xaxis, yaxis, zaxis, df)
+        self.dataframe = self._yaxis_total(yaxis, df)
 
-    def __xaxis_summary(self, source_dataframe, xaxis, yaxis, zaxis, ctdf):
+    def _xaxis_total(self, source_dataframe, xaxis, yaxis, zaxis, ctdf):
         coordinates = []
         for _ in ctdf.columns:
             coordinates.append(self.xaxis[-1])
 
         ## CREATE SUBTOTALS FOR EACH COLUMNS
-        for i in range(0, len(self.visible_xaxis_summary)):
+        for i in range(0, len(self.xaxis_total)):
             subtotal = source_dataframe.groupby(xaxis[:i+1] + yaxis)
             subtotal = subtotal.sum()[zaxis].unstack(xaxis[:i+1])
 
             for col in subtotal.columns:
                 scolumns = col + (col[-1],) * (len(xaxis) - len(col) + 1)
                 ctdf[scolumns] = subtotal[col]
-                coordinates.append(self.visible_xaxis_summary[i])
+                coordinates.append(self.xaxis_total[i])
         ## END
 
         ## CREATE COLUMNS GRAND TOTAL
@@ -68,9 +62,9 @@ class Crosstab(object):
         ## END
 
         ## SORT COLUMNS AND RETURN SORTED DF
-        sorted_columns = self.__sort_axis(ct.columns,
-                                          self.visible_xaxis_summary,
-                                          coordinates, 'x')
+        sorted_columns = self._sort_axis(ct.columns,
+                                         self.xaxis_total,
+                                         coordinates, 'x')
         zas = self.zaxis * (len(sorted_columns) / len(self.zaxis))
         self.coordinates['z'] = zas
         sorted_columns = map(lambda x: x[0][:-1] + (x[1],),
@@ -78,19 +72,19 @@ class Crosstab(object):
         return ct.reindex_axis(axis=1, labels=sorted_columns)
         ## END
 
-    def __yaxis_summary(self, yaxis, df):
+    def _yaxis_total(self, yaxis, df):
         coordinates = []
         for _ in df.index:
             coordinates.append(self.yaxis[-1])
 
         ## CREATE SUBTOTALS FOR EACH INDEX
         subtotals = []
-        for i in range(len(self.visible_yaxis_summary)):
+        for i in range(len(self.yaxis_total)):
             for idx in set([x[:i+1] for x in df.index]):
                 sindex = idx + (idx[-1],) * (len(yaxis) - len(idx))
                 stotal = pd.DataFrame({sindex: df.ix[idx].sum()}).T
                 subtotals.append(stotal)
-                coordinates.append(self.visible_yaxis_summary[i])
+                coordinates.append(self.yaxis_total[i])
         ## END
 
         ## CREATE INDEX GRAND TOTAL
@@ -102,13 +96,13 @@ class Crosstab(object):
 
         ## SORT THE INDEX AND RETURN SORTED DF
         df = pd.concat([df] + subtotals)
-        sorted_index = self.__sort_axis(df.index,
-                                        self.visible_yaxis_summary,
-                                        coordinates, 'y')
+        sorted_index = self._sort_axis(df.index,
+                                       self.yaxis_total,
+                                       coordinates, 'y')
         return df.reindex_axis(axis=0, labels=sorted_index)
         ## END
 
-    def __sort_axis(self, labels, visible_axis, coordinates, axis):
+    def _sort_axis(self, labels, visible_axis, coordinates, axis):
         sorter = []
         for i, l in enumerate(labels):
             if coordinates[i] in visible_axis:
@@ -128,16 +122,3 @@ class Crosstab(object):
             sorted_index.append(lex)
             self.coordinates[axis].append(coordinates[lx])
         return sorted_index
-
-    def __extend(self, extmodule):
-        if extmodule:
-            extmodule = imp.load_source(extmodule[0], extmodule[1])
-            extmodule.extend(self)
-            self.datasets = extmodule.datasets(self)
-            self.conditional_style = extmodule.conditional_style
-        self.values_labels = self.__values_labels(self.dataframe)
-
-    def __values_labels(self, ct):
-        if isinstance(ct.columns, pd.MultiIndex):
-            return map(lambda x: x[-1], ct.columns)
-        return ct.columns
